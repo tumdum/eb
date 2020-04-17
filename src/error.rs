5321a113f176cd747934ef5e6bb463070bf4bc72
Kristofer Rye <kristofer.rye@gmail.com>
2020-04-17 11:00:18 -0500

    crate: Extract error into its own mod
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

---
+++ src/error.rs
@@ -0,0 +1,34 @@
+use core::{
+	fmt::{self, Debug, Display, Formatter},
+	result
+};
+
+#[derive(PartialEq)]
+
+pub enum Error {
+	NoCommandGiven,
+	ChildProcessTerminatedWithSignal,
+}
+
+impl Debug for Error {
+	fn fmt(&self, f: &mut Formatter) -> result::Result<(), fmt::Error> {
+		write!(
+			f,
+			"{}",
+			match self {
+				Self::NoCommandGiven => "no command given",
+				Self::ChildProcessTerminatedWithSignal => "child process terminated with signal",
+			}
+		)
+	}
+}
+
+impl Display for Error {
+	fn fmt(&self, f: &mut Formatter) -> result::Result<(), fmt::Error> {
+		Debug::fmt(self, f)
+	}
+}
+
+impl std::error::Error for Error {}
+
+pub type Result<T> = result::Result<T, Error>;
